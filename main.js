8a8f8984788459e9b2b58a7d067f46df4a65fabd
SaniyaMukhit <saniya.mukhitkyzy@nu.edu.kz>
2022-12-04 21:53:20 +0600

    Update

--- main.js
+++ main.js
@@ -18,12 +18,4 @@ function scrollToTop() {
 }
 
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+
